2cfd0c5b5451b04752acb18d81081ce5735f5a6f
John Strunk <jstrunk@redhat.com>
2020-02-13 15:10:09 -0500

    Add script to properly regenerate helm index.yaml
    This fixes the problem where existing charts were having their `created`
    timestamp updated when a new chart is added.
    
    Signed-off-by: John Strunk <jstrunk@redhat.com>

--- Makefile
+++
@@ -1,3 +0,0 @@
-docs/index.yaml: docs/*.tgz Makefile
-	rm -rf docs/_site
-	helm repo index docs --url https://backube.github.io/helm-charts
---
+++ build-index.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+set -e -o pipefail
+
+REPO_URL="https://backube.github.io/helm-charts"
+
+SCRIPT_NAME="$(basename "$0")"
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+CHART_DIR="${SCRIPT_DIR}/docs"
+
+function usage {
+    cat - <<USAGE
+Usage:
+  $1 <new_chart.tgz>
+
+  This will add new_chart.tgz to the index.yaml file and move it into position
+  in the docs directory (if necessary). After running this script, commit both
+  the modified index.yaml and the chart archive (in its new locaton).
+USAGE
+}
+
+if [[ $# -ne 1 ]]; then
+    usage $SCRIPT_NAME
+    exit 1
+fi
+NEW_CHART="$1"
+
+MERGE_DIR="$(mktemp -d)"
+cp "$NEW_CHART" "$MERGE_DIR"
+helm repo index --url "$REPO_URL" --merge "${CHART_DIR}/index.yaml" "${MERGE_DIR}"
+cp "${MERGE_DIR}/$(basename "${NEW_CHART}")" "${CHART_DIR}"
+cp "${MERGE_DIR}/index.yaml" "${CHART_DIR}"
+
+rm -rf "${MERGE_DIR}"
